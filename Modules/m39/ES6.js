a7acd45968422fd189169517d18044a33b48653d
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-17 18:38:14 +0500

    chore: refine template literals example in ES6.js for clarity

--- Modules/m39/ES6.js
+++ Modules/m39/ES6.js
@@ -3,5 +3,10 @@
 // Template Literals:
 // Old Way:
 const name = "Khubaib"
-console.log("Hello: ", name)
+console.log("Hello",name)
+
+// New way:
+console.log(`Hello ${name}`)
+
+
 // How to restructure the Arrays and Objects
