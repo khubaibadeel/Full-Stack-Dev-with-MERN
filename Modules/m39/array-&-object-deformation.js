8a609eba65328bf6341c4954bd63ed6f8a8f9bc4
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-18 18:28:29 +0500

    chore: add console output examples for array deconstruction

--- Modules/m39/array-&-object-deformation.js
+++ Modules/m39/array-&-object-deformation.js
@@ -4,6 +4,9 @@ let fruits =["Apple", "Banana","Mango"]
 
 let [first,second] = fruits
 
-console.log(first, second)
+console.log(first, second)   // Output: Apple Banana
+
 // To skip the second value,
+
 let [first_fruit, ,second_fruit] = fruits
+console.log(first_fruit,second_fruit)   // Output: Apple Mango
