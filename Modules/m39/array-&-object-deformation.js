bbb63ca9cf151c761b903a88769d2535a80b9aea
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-18 18:26:55 +0500

    chore: restore array and object deformation example

---
+++ Modules/m39/array-&-object-deformation.js
@@ -0,0 +1,9 @@
+// How to restructure the Arrays
+
+let fruits =["Apple", "Banana","Mango"]
+
+let [first,second] = fruits
+
+console.log(first, second)
+// To skip the second value,
+let [first_fruit, ,second_fruit] = fruits
--- Modules/m39/array-deformation.js
+++
@@ -1,7 +0,0 @@
-// How to restructure the Arrays and Objects
-
-let fruits =["Apple", "Banana","Mango"]
-
-let [first,second] = fruits
-
-console.log(first, second)
